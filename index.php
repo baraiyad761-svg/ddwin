36283ac61201ed9a70666ec6dc48a3a1a0bfd5a1
baraiyad761-svg <baraiyad761@gmail.com>
2026-09-25 12:45:08 +0530

    Update index.php

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
 <button class="red" onclick="play('red')">RED</button>
 <button class="violet" onclick="play('violet')">VIOLET</button>
 <p>Balance: ₹ <span id="bal">1000</span></p>
+ <a href="deposit.php"><button style="background:#3498db; width:90%; padding:14px; border-radius:10px; color:#fff; font-weight:bold; border:none; margin-top:10px;">RECHARGE / DEPOSIT</button></a>
 <button class="withdraw" onclick="withdraw()">WITHDRAWAL</button>
 </div>
 <script>
